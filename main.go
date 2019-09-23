9a5856ff9035bcb4c703045567fb8a95fa8ac072
gugutz <gugutz@gmail.com>
2019-09-23 01:07:57 -0300

    add REST style routes in main file using Mux

--- main.go
+++ main.go
@@ -7,15 +7,12 @@ import (
 
 	"github.com/gorilla/mux"
 
-	// imports with the dot on tbhe begginging allow to use its methods without the package name. example: `Method` instead of `package.Method`
 	. "github.com/gugutz/stairs-backend-challenge/config"
 	. "github.com/gugutz/stairs-backend-challenge/dao"
 
-	// here i declare the variable that holds the imported package right on the import line
 	router "github.com/gugutz/stairs-backend-challenge/router"
 )
 
-// import database access object from project
 var dao = DAO{}
 var config = Config{}
 
@@ -30,13 +27,17 @@ func init() {
 
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/wines/{id}", router.Get).Methods("GET")
-	r.HandleFunc("/wines/{id}/edit", router.Update).Methods("POST")
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/wines/new", router.Create).Methods("POST")
 	r.HandleFunc("/wines", router.GetAll).Methods("GET")
-	http.Handle("/", r)
+	r.HandleFunc("/wines", router.GetAll).Methods("POST")
+	r.HandleFunc("/wines/{id}", router.Get).Methods("GET")
+	r.HandleFunc("/wines/{id}/edit", router.Update).Methods("PUT")
+	r.HandleFunc("/wines/{id}", router.DeleteOne).Methods("DELETE")
+	r.HandleFunc("/", router.RootHandler).Methods("GET")
 	
 	var port = ":3000"
 	fmt.Println("Server running in port:", port)
 	log.Fatal(http.ListenAndServe(port, r))
 }
+
